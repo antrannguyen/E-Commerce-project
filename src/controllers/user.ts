680a69eb4f820e0975ee951d3c90b75af5e11bf3
Annine <annine@Annines-Air.home>
2020-06-14 07:36:33 +0300

    fixed the required error

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import {validationResult} = require('express-validator')
+import { validationResult } from 'express-validator'
 
 import User from '../models/User'
 import UserService from '../services/user'
@@ -9,6 +9,7 @@ import {
   InternalServerError,
   UnauthorizedError,
 } from '../helpers/apiError'
+import { any } from 'bluebird'
 
 export const registerUser = async (
   req: Request,
@@ -16,17 +17,15 @@ export const registerUser = async (
   next: NextFunction
 ) => {
   try {
-   
-    console.log('body', req.body)
-    res.send("work?")
     const errors = validationResult(req)
-    if (!errors.isEmpty() ){
-      // next(new BadRequestError('Invalid Request', error))
-      return next(res.status(400).json({errors: errors.array()}))
+    console.log('body', req.body)
+    res.send('work?')
+    if (!errors.isEmpty()) {
+      return next(res.status(400).json({ errors: errors.array() }))
     }
     res.json(await UserService.findAll())
   } catch (error) {
-    const errors = validationResult(req)
+    // const errors = validationResult(req)
     // if (!errors.isEmpty() ){
     //   // next(new BadRequestError('Invalid Request', error))
     //   return res.status(400).json({errors: errors.array()})
